0182d8857cdc83662e2be63a8fa17ee68fa12a71
dariakrajewska <110968346+dariakrajewska@users.noreply.github.com>
2022-08-10 14:20:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var test = prompt("Podaj swoje imie")
 // prompt(tekst) zawiera wpisany tekst
-alert("Witaj " + test + "! Przedstawiam Ci moje CV.")
+if (test) {
+alert("Witaj " + test + "! Przedstawiam Ci moje CV.")}
+else alert("Nie wiem kim jesteś, ale chciałam pokazać Ci moje CV :)")
 //Alertuje wpisany tekst
